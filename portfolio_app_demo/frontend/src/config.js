98c7dd406b8315227b1bf4928d84620e0dd02571
ChaisiriTum <138801167+ChaisiriTum@users.noreply.github.com>
2025-12-12 13:23:43 +0700

    Update config.js

--- portfolio_app_demo/frontend/src/config.js
+++ portfolio_app_demo/frontend/src/config.js
@@ -1,5 +1,4 @@
 // frontend/src/config.js
-export const API_URL = (import.meta && import.meta.env && import.meta.env.VITE_BACKEND_URL)
-  ? import.meta.env.VITE_BACKEND_URL.replace(/\/$/, '') // เอา / ท้ายออก
+export const API_URL = import.meta.env.VITE_BACKEND_URL
+  ? import.meta.env.VITE_BACKEND_URL.replace(/\/$/, '') 
   : '';
-
